a50a227f9620e54d168082341fbe9c6aefa4b9fc
Ismail Dawoodjee <58870992+ismaildawoodjee@users.noreply.github.com>
2021-05-10 02:54:07 +0800

    Added additional imports and corrected SQLite config

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from sqlite3 import Connection as SQLite3Connection
 from datetime import datetime
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # Flask app
 app = Flask(__name__)
 
 # Configure SQLite database file
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
-app.config["SQL_TRACK_MODIFICATIONS"] = 0
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
