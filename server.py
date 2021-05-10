c6d05fc0d1b51abf76ea230b04451e1031a20ca6
Ismail Dawoodjee <58870992+ismaildawoodjee@users.noreply.github.com>
2021-05-10 15:51:43 +0800

    Added placeholder functions and defined create_user function

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ now = datetime.now()
 
 # Class models for each table in the database
 class User(db.Model):
-    """Model for creating the User table with columns"""
+    """Model for creating the User table with columns."""
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
@@ -41,3 +41,72 @@ class User(db.Model):
     address = db.Column(db.String(256))
     phone = db.Column(db.String(64))
     posts = db.relationship("BlogPost")  # foreign key for BlogPost
+
+class BlogPost(db.Model):
+    """Table class for BlogPost model, 
+    since this is going to be an API for blogs. Should inherit db.Model.
+    """
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+    body = db.Column(db.String(256))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+@app.route("/user", methods=["POST"])
+def create_user():
+    """Create route to route the POST request to this function,
+    whenever the `/user` rule is appended to the URL. In simple
+    terms, this function creates a new user and puts the data on the DB.
+    """
+    data = request.get_json()  # request object provided by Flask
+    new_user = User(
+        name=data["name"],
+        email=data["email"],
+        address=data["address"],
+        phone=data["phone"]
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created"}), 200
+
+@app.route("/user/descending_id", methods=["GET"])
+def get_all_users_descending():
+    pass
+
+@app.route("/user/ascending_id", methods=["GET"])
+def get_all_users_ascending():
+    pass
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_one_user(user_id):
+    pass
+
+
+@app.route("/user/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    pass
+
+
+@app.route("/blog_post/<user_id>", methods=["POST"])
+def create_blog_post(user_id):
+    pass
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_all_blog_posts(user_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_one_blog_post(user_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
+def delete_blog_post(blog_post_id):
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
